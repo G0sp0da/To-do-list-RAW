fa5004bf76712415818477d199100ad63c136f05
Petar Kolić <100611675+G0sp0da@users.noreply.github.com>
2022-03-11 12:58:29 +0100

    Add files via upload

---
+++ actions.js
@@ -0,0 +1,26 @@
+
+let myForm = document.getElementById("myForm")
+let myField = document.getElementById("myField")
+let myList = document.getElementById("myList")
+
+myForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  createItem(myField.value)
+  
+  
+})
+
+function createItem(x) {
+  let myHTML = `<li id="myTodo">${x.toUpperCase()}<button onclick="deleteItem(this)">Delete</button></li>`
+  myList.insertAdjacentHTML("beforeend", myHTML)
+  myField.value=""
+  myField.focus()
+  
+  
+
+}
+
+function deleteItem(elementToDelete) {
+elementToDelete.parentElement.remove()
+}
+
